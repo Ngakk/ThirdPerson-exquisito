5f7accc21f05e2e2466b9ceccc4b63fb4032e096
AgustinV08 <32682024+AgustinV08@users.noreply.github.com>
2018-07-04 11:59:42 -0500

    Balas Enemigas (funcion CambiarDireccion)

---
+++ Assets/Agustin/Bala_Enemiga.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+	public class Bala_Enemiga : MangosBehaviour 
+	{
+		
+		float tiempo = 5.0f;
+		float delay;
+		
+		Rigidbody rigi;
+	
+		// Use this for initialization
+		void Start () 
+		{
+			rigi = gameObject.GetComponent<Rigidbody>();
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			delay -= Time.deltaTime;
+			
+			if(delay <= 0.0f)
+			{
+				gameObject.SetActive(false);
+				
+				delay = tiempo;
+			}
+		}
+		
+		public void CambiarDireccion()
+		{
+			rigi.velocity = -rigi.velocity;
+		}
+	}
+}
--- Assets/Agustin/Enemigo.cs
+++ Assets/Agustin/Enemigo.cs
@@ -2,20 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Mangos{
+namespace Mangos
+{
 
 	public class Enemigo : MangosBehaviour
 	{
+		float disparo = 1.0f;
+		float delay;
+		
+		// Pool Bala
+		List<GameObject> Bala = new List<GameObject>();
+		
+		GameObject Generar_Bala()
+		{
+			for (int i = 0; i < Bala.Count; i++)
+			{
+				if(Bala[i].activeSelf == false)
+				{
+					Bala[i].SetActive(true);
+					Bala[i].transform.position = transform.position;
+					Bala[i].GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+					return Bala[i];
+				}
+			}
+			
+			GameObject Prefab_Bala = Resources.Load<GameObject>("Sphere");
+			GameObject go = Instantiate(Prefab_Bala, transform.position, Quaternion.identity);
+			Bala.Add(go);
+			return go;
+		}
+		
 
 		// Use this for initialization
-		void Start () {
-			
+		void Start () 
+		{
+			delay = disparo;
 		}
 		
 		// Update is called once per frame
 		void Update () 
 		{
+			delay -= Time.deltaTime;
 			
+			if(delay <= 0.0f)
+			{
+				GameObject go = Generar_Bala();
+				
+				go.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+				
+				delay = disparo;
+			}
 		}
 	}
 }
