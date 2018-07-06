9d361d5a47c8de86eaf351f710a159c7b68da606
AgustinV08 <32682024+AgustinV08@users.noreply.github.com>
2018-07-06 00:05:57 -0500

    NavMesh

---
+++ Assets/Agustin/NavMesh.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh : MonoBehaviour {
+	
+	public NavMeshAgent nav;
+	public Transform Obj;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		nav.SetDestination(Obj.position);
+	}
+}
