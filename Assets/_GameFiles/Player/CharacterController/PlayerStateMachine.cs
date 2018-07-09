50f329f216b99d7d9663d33d17d18edbaaa73679
Ngak <ngakwastaken@gmail.com>
2018-07-09 14:56:00 -0500

    controller animator and sub state machine

---
+++ Assets/_GameFiles/Player/CharacterController/PlayerStateMachine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+	    animator.SetInteger("IdleId", Random.Range(0, 4));
+    }
+}
