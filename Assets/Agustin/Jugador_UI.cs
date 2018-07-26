b01fc547469d4b8506229d2acb7da4e9b3504fce
AgustinV08 <32682024+AgustinV08@users.noreply.github.com>
2018-07-26 11:55:09 -0500

    UI

---
+++ Assets/Agustin/Jugador_UI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Jugador_UI : MonoBehaviour 
+{
+	
+	float hp;
+	
+	public Image imagen;
+
+	// Use this for initialization
+	void Start () 
+	{
+		hp = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnCollisionEnter(Collision _col)
+	{
+		if(_col.collider.CompareTag("Bala_Enemigo"))
+		{
+			hp -= 0.1f;
+			imagen.fillAmount = hp;
+		}
+	}
+}
