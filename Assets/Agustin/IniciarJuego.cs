6493f3b01d7760100ab29cc1f084a5da95f4d617
AgustinV08 <32682024+AgustinV08@users.noreply.github.com>
2018-07-29 16:09:33 -0500

    Boton

---
+++ Assets/Agustin/IniciarJuego.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IniciarJuego : MonoBehaviour {
+	
+	public void Juego()
+	{
+		SceneManager.LoadScene("NavMesh");
+	}
+}
